6489387f332ca538c2f0b3342924f622f3cc2541
Vincent Howard <vincenguyenhoward@gmail.com>
2022-07-24 00:46:02 -0600

    learned about dynamically creating different environemnts for each contract as well as mocks for a chainlink contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,44 @@
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-ethers'
-import 'hardhat-gas-reporter'
-import 'dotenv/config'
-import 'solidity-coverage'
-import 'hardhat-deploy'
-import 'solidity-coverage'
-import { HardhatUserConfig } from 'hardhat/config'
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "solidity-coverage"
+import { HardhatUserConfig } from "hardhat/config"
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHER_SCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COIN_MARKET_CAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
 const config: HardhatUserConfig = {
-    solidity: '0.8.9',
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31335,
+        },
+        rinkeby: {
+            chainId: 4,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.9",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
 
 export default config
